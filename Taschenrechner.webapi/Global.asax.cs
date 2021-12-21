bddf2153c80f5e5d11070e580e23e6e958643dad
Ejub Veliu <eveliu@online.gibz.ch>
2021-12-21 15:56:50 +0100

    Hello World funktioniert

---
+++ Taschenrechner.webapi/Global.asax.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Taschenrechner.webapi
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+            });
+
+
+            RouteTable.Routes.MapHttpRoute(name: "HelloWorld", routeTemplate: "HelloWorld/{action}",
+            defaults: new
+            {
+                controller = "HelloWorld",
+                id = System.Web.Http.RouteParameter.Optional
+            }
+            );
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Taschenrechner.webapi/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Taschenrechner.webapi
+{
+    public class HelloWorldController : ApiController
+    {
+        // GET api/<controller>/5
+        [HttpGet]
+        [ActionName("GetHelloWorld")]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+    }
+}
